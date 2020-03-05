77b62303aab3763e90bd1d019b925bb132b4abb4
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-05 14:40:35 +0530

    created Player class with constructor

---
+++ Player.js
@@ -0,0 +1,22 @@
+class Player
+{
+    constuctor(name, symbol)
+    {
+        this.name = name;
+        this.symbol = symbol;
+    }
+    get symbol()
+    {
+        return this.symbol;
+    }
+    get position()
+    {
+        // input position
+        return position;
+    }
+}
+
+module.exports =
+{
+    Player : Player;
+}
