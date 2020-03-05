cad983bc4e4e144f7bf7858786252861928a7256
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-05 15:18:29 +0530

    added displayStatus, startGame, takePosition functions

--- GameController.js
+++ GameController.js
@@ -4,9 +4,30 @@ playerObject = require('./Player');
 class Controller
 {
     gameFinished = false;
+    turnsRemaining = 9;
     board = new boardObject.Board();
     players = [new playerObject.Player("2nd Player", 'O'), new playerObject.Player("1st Player", 'X')];
 
+    function startGame()
+    {
+        while(turnsRemaining > 0 && !gameFinished)
+        {
+            var position = takePosition();
+            gameFinished = board.tester(position);
+            turnsRemaining --;
+        }
+        displayResult();
+    }
+
+    function takePosition()
+    {
+        return players[turnsRemaining%2].position;
+    }
+
+    function displayResult()
+    {
+        gameFinished ? console.log(players[(turnsRemaining+1)%2] + " won") : console.log("Draw Match");
+    }
 }
 
 module.exports ={
