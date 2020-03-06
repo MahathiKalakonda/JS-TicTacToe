848c3a6c82d6516c0c48f237341a619af39a115d
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-06 19:32:31 +0530

    changed board intialisation

--- Board.js
+++ Board.js
@@ -1,12 +1,7 @@
 class Board
 {
-    board = [];
+    board = ['','','','','','','','',''];
     conditions = [[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
-
-    constructor()
-    {
-        this.board[9] = 0;
-    }
 
     setSymbol(position, symbol)
     {
