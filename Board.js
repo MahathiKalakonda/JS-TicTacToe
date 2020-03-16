0b776be088e18bae829820bf785467ba51ebe498
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-16 11:26:40 +0530

    added contructor

--- Board.js
+++ Board.js
@@ -1,18 +1,20 @@
 class Board
 {
-    board = ['','','','','','','','',''];
-    conditions = [[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
-
+    constructor()
+    {
+        this.board = ['','','','','','','','',''];
+        this.conditions = [[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
+    }
+    
     setSymbol(position, symbol)
     {
-//        this.board[position] ? console.log('error',) :
         this.board[position] = symbol;
         this.displayBoard();
     }
 
     tester(position, symbol)
     {
-        var conditionsWithPosition = this.conditions.filter(condition => condition.indexOf(position) != 0), board = this.board;
+        var conditionsWithPosition = this.conditions.filter(condition => condition.indexOf(position) !== 0), board = this.board;
         return conditionsWithPosition.some(function(condition)
         {
             return condition.every(cell => symbol == board[cell]);
