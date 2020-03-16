9f8677cdda25c4d6e8c7183a1db5a9f75787f5d7
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-16 11:15:28 +0530

    added contructor

--- GameController.js
+++ GameController.js
@@ -3,19 +3,23 @@ playerObject = require('./Player');
 
 class Controller
 {
-    gameFinished = false;
-    turnsRemaining = 9;
-    board = new boardObject.Board();
-    players = [new playerObject.Player("2nd Player", 'O'), new playerObject.Player("1st Player", 'X')];
+    constructor()
+    {
+        gameFinished = false;
+        turnsRemaining = 9;
+        board = new boardObject.Board();
+        players = [new playerObject.Player("2nd Player", 'O'), new playerObject.Player("1st Player", 'X')];
+    }
 
     startGame()
     {
+        //this.board.board = 'something'
         while(this.turnsRemaining > 0 && !this.gameFinished)
         {
             this.play()
         }
         this.displayResult();
-        return;
+        return 0;
     }
 
     play()
@@ -34,7 +38,7 @@ class Controller
     displayResult()
     {
         this.gameFinished ? console.log(this.players[(this.turnsRemaining+1)%2].getName() + " won") : console.log("Draw Match");
-        return;
+        return 0;
     }
 }
 
