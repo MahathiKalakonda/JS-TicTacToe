80bcb339015b6e2e24f25ec9ba6afdc2ae3ba19e
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-17 23:52:23 +0530

    added myFunction method

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,8 +1,8 @@
-const controller = require('./GameController');
-const playerObject = require('./Player');
-
-var players = [new playerObject.Player("2nd Player", 'O'), new playerObject.Player("1st Player", 'X')];
-var ticTacToeController = new controller.GameController(players);
+//const controller = require('./GameController');
+//const playerObject = require('./Player');
+//
+//var players = [new playerObject.Player("2nd Player", 'O'), new playerObject.Player("1st Player", 'X')];
+//var ticTacToeController = new controller.GameController(players);
 //ticTacToeController.startGame();
 
 
@@ -13,6 +13,6 @@ function myFunction(position)
   var symbols = ['O' , 'X'];
   document.getElementById(position).innerHTML = symbols[turnsRemaining%2];
   turnsRemaining--;
-  ticTacToeController.play();
+//  ticTacToeController.play();
   document.getElementById("playerName").innerHTML = turnsRemaining>0 ? names[turnsRemaining%2] : "Game Over";
 }
