7c0a679abcb5c423853a44e13156e9c06eabbb6a
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-17 12:52:58 +0530

    passed player objects as parameters to contructor

--- PlayTicTacToe.js
+++ PlayTicTacToe.js
@@ -1,4 +1,6 @@
 controller = require('./GameController');
+const playerObject = require('./Player');
 
-var ticTacToeController = new controller.GameController();
+var players = [new playerObject.Player("2nd Player", 'O'), new playerObject.Player("1st Player", 'X')];
+var ticTacToeController = new controller.GameController(players);
 ticTacToeController.startGame();
