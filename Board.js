41110f4e5ed0b379de4f418ffa03cddd95bd78de
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-19 00:46:18 +0530

    added getBoard method

--- Board.js
+++ Board.js
@@ -2,27 +2,32 @@ class Board
 {
     constructor()
     {
-        this.board = ['','','','','','','','',''];
         this.conditions = [[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
     }
 
     setSymbol(position, symbol)
     {
-        this.board[position] = symbol;
+        document.getElementById(position).innerHTML = symbol;
+        return 0;
+    }
+
+    getBoard()
+    {
+        var board = ['','','','','','','','',''];
+        return board.map(function(element, index)
+            {
+                return document.getElementById(index).innerHTML;
+            })
     }
 
     isGameFinished(position, symbol)
     {
-        var conditionsWithPosition = this.conditions.filter(condition => condition.indexOf(position) !== 0), board = this.board;
-        return conditionsWithPosition.some(function(condition)
+        var board = this.getBoard();
+        var conditionsWithPosition = this.conditions.filter(condition => condition.indexOf(position) !== 0);
+        return status =  conditionsWithPosition.some(function(condition)
         {
             return condition.every(cell => symbol == board[cell]);
         });
     }
 
 }
-
-module.exports =
-{
-    Board : Board
-};
