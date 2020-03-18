393faa986fb395dbdcedf1532985331b72552e95
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-19 00:47:38 +0530

    added getFinalStatus getCurrentPlayer methods

--- GameController.js
+++ GameController.js
@@ -1,39 +1,31 @@
-const boardObject = require('./Board');
-const playerObject = require('./Player');
-//const inputAndOutputObject = require('./consoleAndPrompt');
-
 class GameController
 {
     constructor(players)
     {
         this.gameFinished = false;
-        this.turnsRemaining = 9;
-//        this.promptAndDisplay = new inputAndOutputObject.consoleAndPrompt();
-        this.board = new boardObject.Board();
+        this.board = new Board();
         this.players = players;
     }
 
-    startGame()
+    play(position,turnsRemaining)
     {
-        while(this.turnsRemaining > 0 && !this.gameFinished)
-        {
-            this.play()
-        }
-//        this.promptAndDisplay.displayResult(this.players[(this.turnsRemaining+1)%2].getName(), this.gameFinished);
-        return 0;
+        this.board.setSymbol(position, this.players[turnsRemaining%2].getSymbol());
+        this.gameFinished = this.board.isGameFinished(position, this.players[turnsRemaining%2].symbol);
+        return;
     }
 
-    play()
+    getGameFinished()
     {
-        var position = this.promptAndDisplay.getPosition(this.players[this.turnsRemaining%2].getName());
-        this.board.setSymbol(position, this.players[this.turnsRemaining%2].getSymbol());
-//        this.promptAndDisplay.displayBoard(this.board.board);
-        this.gameFinished = this.board.isGameFinished(position, this.players[this.turnsRemaining%2].symbol);
-        this.turnsRemaining --;
+        return this.gameFinished;
     }
 
-}
+    getFinalStatus(turnsRemaining)
+    {
+        return this.gameFinished ? this.players[(turnsRemaining+1)%2].name+" won" : "Draw Match";
+    }
 
-module.exports ={
-GameController:GameController
+    getCurrentPlayer(turnsRemaining)
+    {
+        return this.players[turnsRemaining%2].name;
+    }
 }
