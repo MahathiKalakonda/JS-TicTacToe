718eb58355baa8b795acfef9dc2f1634dbde6d75
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-19 13:06:39 +0530

    added getWinningCondition method

--- Board.js
+++ Board.js
@@ -2,6 +2,7 @@ class Board
 {
     constructor()
     {
+        this.winningCondition = [];
         this.conditions = [[0,1,2],[0,3,6],[0,4,8],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[2,4,6]];
     }
 
@@ -22,12 +23,19 @@ class Board
 
     isGameFinished(position, symbol)
     {
-        var board = this.getBoard();
+        var board = this.getBoard(), winningCondition = [];
         var conditionsWithPosition = this.conditions.filter(condition => condition.indexOf(position) !== 0);
-        return status =  conditionsWithPosition.some(function(condition)
+        var status =  conditionsWithPosition.some(function(condition)
         {
+            winningCondition = condition;
             return condition.every(cell => symbol == board[cell]);
         });
+        this.winningCondition = winningCondition;
+        return status;
     }
 
+    getWinningCondition()
+    {
+        return this.winningCondition;
+    }
 }
