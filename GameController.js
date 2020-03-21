476f6b134b7b540b19b6156eb4afbb4d5fd7b897
MahathiKalakonda <mahathi.kalakonda@thoughtworks.com>
2020-03-21 11:47:22 +0530

    removed display statements

--- GameController.js
+++ GameController.js
@@ -12,7 +12,37 @@ class GameController
     {
         this.inputAndOutput.displaySymbol(position, this.players[turnsRemaining%2].getSymbol());
         this.gameFinished = this.board.isGameFinished(position, this.players[turnsRemaining%2].symbol);
-        return;
+        this.updateStatus(turnsRemaining);
+    }
+
+    updateStatus(turnsRemaining)
+    {
+        this.gameFinished || turnsRemaining == 1 ? this.updateFinalStatus(turnsRemaining) : this.updateCurrentPlayer(turnsRemaining);
+        this.updateScores(turnsRemaining);
+        this.highlightWinningPattern();
+    }
+
+    updateFinalStatus(turnsRemaining)
+    {
+        var finalStatus = this.gameFinished ? this.players[turnsRemaining%2].name+" won" : "Draw Match";
+        this.inputAndOutput.displayFinalStatus(finalStatus);
+    }
+
+    updateCurrentPlayer(turnsRemaining)
+    {
+        this.inputAndOutput.displayCurrentPlayer(this.players[(turnsRemaining+1)%2].name);
+    }
+
+    updateScores(turnsRemaining)
+    {
+        if(this.gameFinished)
+            scores[turnsRemaining%2]++;
+        this.inputAndOutput.displayUpdatedScore(this.players[turnsRemaining%2].name, scores[turnsRemaining%2]);
+    }
+
+    highlightWinningPattern()
+    {
+        this.inputAndOutput.highlightBackground(this.board.getWinningCondition());
     }
 
     getGameFinished()
@@ -20,32 +50,14 @@ class GameController
         return this.gameFinished;
     }
 
-    getFinalStatus(turnsRemaining)
-    {
-        return this.gameFinished ? this.players[(turnsRemaining+1)%2].name+" won" : "Draw Match";
-    }
-
-    getCurrentPlayer(turnsRemaining)
-    {
-        return this.players[turnsRemaining%2].name;
-    }
-
-    updateScores(turnsRemaining)
-    {
-        if(this.gameFinished)
-            scores[(turnsRemaining+1)%2]++;
-        this.inputAndOutput.displayUpdatedScore(this.players[(turnsRemaining+1)%2].name, scores[(turnsRemaining+1)%2]);
-    }
-
-    highlightWinningPattern()
-    {
-        var winningCondition = this.board.getWinningCondition();
-        this.inputAndOutput.highlightBackground(winningCondition);
-    }
-
     resetDisplay()
     {
         this.inputAndOutput.emptyBoard();
         this.inputAndOutput.resetPlayerStatus();
+    }
+
+    getBoard()
+    {
+        return this.inputAndOutput.getGameBoard();
     }
 }
